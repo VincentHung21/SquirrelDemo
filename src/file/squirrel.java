3703258f6a40d74d6dd6bc57e43949c04a3db4f9
Vincent Hung <vincent.hung21@ycdsbk12.ca>
2020-12-08 14:33:33 +0000

    compile error

---
+++ src/file/squirrel.java
@@ -0,0 +1,23 @@
+public class squirrel{
+  int intage;
+  int intweight;
+  int intteethlength;
+  String strcolor;
+
+  public squirrel(int newintage, int newintweight, int newintteethlength, String newstrcolor) {
+    this.intage = newintage;
+    this.intweight = newintweight;
+    this.intteethlength = newintteethlength;
+    this.strcolor = newstrcolor;
+  }
+
+  public void jump(int height) {
+    System.out.println("jumped " + height);
+  }
+  public void dig(String location) {
+    System.out.println("digged at" + location);
+  }
+  public void sleep(int duration) {
+    System.out.println("sleeps for " + duration + " minutes");
+  }
+}
